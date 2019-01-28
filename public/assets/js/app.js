00521599144406765c6fe7afb284d2b84f03b89c
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-01-28 07:51:05 -0700

    added modal code to calc

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   $("#submitBtn").on("click", function (event) {
     event.preventDefault();
@@ -9,14 +9,14 @@ $(document).ready(function() {
     var food_source = $("#foodQ").val();
 
     //grab the values from all the recycle items clicked and adds them together
-    var recycleArray =  [];
+    var recycleArray = [];
     $("input[name=recycle]:checked").each(function () {
-      if(this.checked){
+      if (this.checked) {
         recycleArray.push($(this).val())
       };
     });
-    
-    recycleArray = recycleArray.reduce((a, b) => parseInt(a) + parseInt (b), 0);
+
+    recycleArray = recycleArray.reduce((a, b) => parseInt(a) + parseInt(b), 0);
     console.log(recycleArray);
 
     //multiply the two water values together
@@ -28,9 +28,9 @@ $(document).ready(function() {
     var personal_vehicle = $('input[name=vehicle]:checked').val();
     var public_transportation = $('input[name=public]:checked').val();
     var air_travel = $('input[name=flight]:checked').val();
-    
-    var totalFootprint = (parseInt(household_members) + parseInt(home_size) + parseInt(food_choice) + parseInt(food_source) + 
-      waterTotal + parseInt(purchases) + parseInt(waste) + recycleArray + parseInt(personal_vehicle) + 
+
+    var totalFootprint = (parseInt(household_members) + parseInt(home_size) + parseInt(food_choice) + parseInt(food_source) +
+      waterTotal + parseInt(purchases) + parseInt(waste) + recycleArray + parseInt(personal_vehicle) +
       parseInt(public_transportation) + parseInt(air_travel));
 
     var userInput = {
@@ -55,35 +55,62 @@ $(document).ready(function() {
       alert("Please complete all questions in the survey! Thanks!");
     } else {
       // If we have a number for total footprint 
-      sendSurvey(userInput.household_members, userInput.home_size, userInput.food_choice, userInput.food_source, 
-        userInput.waterTotal, userInput.purchases, userInput.waste, userInput.recycle, userInput.personal_vehicle, 
+      sendSurvey(userInput.household_members, userInput.home_size, userInput.food_choice, userInput.food_source,
+        userInput.waterTotal, userInput.purchases, userInput.waste, userInput.recycle, userInput.personal_vehicle,
         userInput.public_transportation, userInput.air_travel, userInput.totalFootprint);
     };
-    
+
     // Send the POST request.
     function sendSurvey(household_members, home_size, food_choice, food_source, waterTotal, purchases, waste, recycleArray, personal_vehicle, public_transportation, air_travel, totalFootprint) {
-    $.post("/api/footprints", {
-      household_members: household_members,
-      home_size: home_size,
-      food_choice: food_choice,
-      food_source: food_source,
-      waterTotal: waterTotal,
-      purchases: purchases,
-      waste: waste,
-      recycle: recycleArray,
-      personal_vehicle: personal_vehicle,
-      public_transportation: public_transportation,
-      air_travel: air_travel,
-      totalFootprint: totalFootprint
-    })
-    .then(function(data) {
-      window.location.replace(data);
-      // If there's an error, handle it by throwing up a bootstrap alert
-    })      
-    .catch(function(err) {
-      res.json(err);
-      console.log(err);
-    });
-  };
+      $.post("/api/footprints", {
+        household_members: household_members,
+        home_size: home_size,
+        food_choice: food_choice,
+        food_source: food_source,
+        waterTotal: waterTotal,
+        purchases: purchases,
+        waste: waste,
+        recycle: recycleArray,
+        personal_vehicle: personal_vehicle,
+        public_transportation: public_transportation,
+        air_travel: air_travel,
+        totalFootprint: totalFootprint
+      })
+        .then(function (data) {
+          // window.location.replace(data);
+
+          $('#myModal').on('show.bs.modal', function (e) {
+            if (!data) return e.preventDefault() // stops modal from being shown
+
+            else if (data.totalFootprint <= 60) {
+              $("#matchInfo").empty();
+              var matchName = $("<p>").html("<strong>" + data.totalFootprint + "<strong>");
+              $("#matchInfo").append(matchName);
+              var message = $("<p>").html("Congratulations! The average consumer has a footprint value of sixty. Check out our resources for new ideas to keep living your green.");
+              $("#matchInfo").append(message);
+            }
+            else if (data.totalFootprint > 60) {
+              $("#matchInfo").empty();
+              var matchName = $("<p>").html("<strong>" + data.totalFootprint + "<strong>");
+              $("#matchInfo").append(matchName);
+              var message = $("<p>").html("The average consumer has a footprint value of sixty. Check out our resources to find easy ways to reduce your carbon footprint and start living your green.");
+              $("#matchInfo").append(message);
+            }
+
+
+          });
+          //this calls the modal to pop up
+          $('#myModal').modal('show')
+          //reloads the page when the modal is dismissed
+          $('#myModal').on('hidden.bs.modal', function (e) {
+            location.reload()
+          })
+          // If there's an error, handle it by throwing up a bootstrap alert
+        })
+      // .catch(function (err) {
+      //   res.json(err);
+      //   console.log(err);
+      // });
+    };
   });
 });
