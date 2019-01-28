f49de70a0ea6ca49b15277b7b810ea49ee40fc3b
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-28 16:33:48 -0700

    working on chart.js

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,12 +1,49 @@
-require(['path/to/chartjs/dist/Chart.js'], function(Chart){
-    var ctx = document.getElementById('myChart').getContext('2d');
+//var db = require("../models");
+//var footprintdata = require("../js/app");
+var Chart = require('dist/Chart.bundle.js');
+
+
+Chart, function(Chart){
+    var ctx = $("#myChart");
 
     var myChart = new Chart(ctx, {
         type: 'bar',
-        data: data,
-        options: options
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
     });
 
+res.render("index", myChart);
 
 
-});
+};
