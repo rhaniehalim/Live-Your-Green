383859c7623192f840f69ea6f7773f537e6410ca
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-28 20:27:05 -0700

    loading sample chart to the page

---
+++ public/assets/js/homeChart.js
@@ -0,0 +1,32 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["Your Footprint", "Average User Footprint", "Low Footprint"],
+        datasets: [{
+            label: '# of Votes',
+            data: [80, 60, 45],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+                'rgba(255, 159, 64, 0.6)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+   
+});
