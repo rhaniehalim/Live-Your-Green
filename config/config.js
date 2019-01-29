c6da27bbd6ec9e1759ef6a700e488008866c07eb
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-01-28 22:10:52 -0700

    jaws changes

--- config/config.js
+++ config/config.js
@@ -18,10 +18,7 @@ require('dotenv').config();
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
