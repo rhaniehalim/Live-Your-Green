c04e1ad4cc09cef19cee1da50484e6701b4b0c86
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-01-29 19:54:28 -0700

    server route coded, not returning object yet

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -7,44 +7,68 @@ $("#userInput").on("submit", function (event) {
 	$('.result-container').empty();
 
 	var searchterm = $("#zipcode").val()
+
+	if (!searchterm) {
+		alert("Please enter your zipcode");
+		return;
+	}
+
+	searchzip(searchterm);
 	//console.log(searchterm);
-	$.get(baseUrl + "earth911.getPostalData?postal_code=" + searchterm + "&country=US&api_key=" + apiKey, function (response) {
-		var data = JSON.parse(response);
-		console.log(data)
+	// $.get(baseUrl + "earth911.getPostalData?postal_code=" + searchterm + "&country=US&api_key=" + apiKey, function (response) {
+	// 	var data = JSON.parse(response);
+	// 	console.log(data)
 
-		$.get(baseUrl + "earth911.searchLocations?latitude=" + data.result.latitude + "&longitude=" + data.result.longitude + "&api_key=" + apiKey + "&max_results=10", function (response2) {
-			var data2 = JSON.parse(response2);
-			console.log(data2);
+	// 	$.get(baseUrl + "earth911.searchLocations?latitude=" + data.result.latitude + "&longitude=" + data.result.longitude + "&api_key=" + apiKey + "&max_results=10", function (response2) {
+	// 		var data2 = JSON.parse(response2);
+	// 		console.log(data2);
 
-			data2.result.forEach(function(result) {
-				$.get(baseUrl + "earth911.getLocationDetails?location_id=" + result.location_id + "&api_key=" + apiKey, function (response3) {
-					var data3 = JSON.parse(response3);
-					// console.log(data3.result[resultObj.location_id]);
-					var locationObj = data3.result[result.location_id];
-					console.log(locationObj);
-					var object = {
-						description: locationObj.description,
-						address: locationObj.address,
-						city: locationObj.city,
-						country: locationObj.country,
-					};
-					
-					var container = $('<article>');
-					// container.css('padding', '15px');
-					var div1 = $(`<div>${ object.description }</div>`);
-					var div2 = $(`<div>${ object.address }</div>`);
-					var div3 = $(`<div>${ object.city }</div>`);
-					var div4 = $(`<div>${ object.country }</div>`);
-					container.append(div1);
-					container.append(div2);
-					container.append(div3);
-					container.append(div4);
-					$('.result-container').append(container)
-				});
-			});
-		});
-	});
+	// 		data2.result.forEach(function(result) {
+	// 			$.get(baseUrl + "earth911.getLocationDetails?location_id=" + result.location_id + "&api_key=" + apiKey, function (response3) {
+	// 				var data3 = JSON.parse(response3);
+	// 				// console.log(data3.result[resultObj.location_id]);
+	// 				var locationObj = data3.result[result.location_id];
+	// 				console.log(locationObj);
+	// 				var object = {
+	// 					description: locationObj.description,
+	// 					address: locationObj.address,
+	// 					city: locationObj.city,
+	// 					country: locationObj.country,
+	// 				};
+
+					// var container = $('<article>');
+					// // container.css('padding', '15px');
+					// var div1 = $(`<div>${ object.description }</div>`);
+					// var div2 = $(`<div>${ object.address }</div>`);
+					// var div3 = $(`<div>${ object.city }</div>`);
+					// var div4 = $(`<div>${ object.country }</div>`);
+					// container.append(div1);
+					// container.append(div2);
+					// container.append(div3);
+					// container.append(div4);
+					// $('.result-container').append(container)
+				// });
+			// });
+		// });
+	// });
 });
+
+
+function searchzip(zipcode) {
+	$.post("/api/recycling", {
+	  zipcode: zipcode
+	}).then(function(data) {
+	  console.log("hitting it?")
+	  console.log(data);
+	  // localStorage.setItem('id', data.id);
+	  // window.location.replace("/index");
+	//   window.location.replace(data);
+	  // If there's an error, log the error
+	}).catch(function(err) {
+	  console.log(err.responseText);
+	  $(".error").text(err.responseText);
+	});
+  }
 
 
 
