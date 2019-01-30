70c84c7f6d4f831f8d621dfc3ad87c97a2208df0
Rhanie Halim <42691016+rhaniehalim@users.noreply.github.com>
2019-01-29 22:44:56 -0700

    Update maps.js

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -14,43 +14,6 @@ $("#userInput").on("submit", function (event) {
 	}
 
 	searchzip(searchterm);
-	//console.log(searchterm);
-	// $.get(baseUrl + "earth911.getPostalData?postal_code=" + searchterm + "&country=US&api_key=" + apiKey, function (response) {
-	// 	var data = JSON.parse(response);
-	// 	console.log(data)
-
-	// 	$.get(baseUrl + "earth911.searchLocations?latitude=" + data.result.latitude + "&longitude=" + data.result.longitude + "&api_key=" + apiKey + "&max_results=10", function (response2) {
-	// 		var data2 = JSON.parse(response2);
-	// 		console.log(data2);
-
-	// 		data2.result.forEach(function(result) {
-	// 			$.get(baseUrl + "earth911.getLocationDetails?location_id=" + result.location_id + "&api_key=" + apiKey, function (response3) {
-	// 				var data3 = JSON.parse(response3);
-	// 				// console.log(data3.result[resultObj.location_id]);
-	// 				var locationObj = data3.result[result.location_id];
-	// 				console.log(locationObj);
-	// 				var object = {
-	// 					description: locationObj.description,
-	// 					address: locationObj.address,
-	// 					city: locationObj.city,
-	// 					country: locationObj.country,
-	// 				};
-
-					// var container = $('<article>');
-					// // container.css('padding', '15px');
-					// var div1 = $(`<div>${ object.description }</div>`);
-					// var div2 = $(`<div>${ object.address }</div>`);
-					// var div3 = $(`<div>${ object.city }</div>`);
-					// var div4 = $(`<div>${ object.country }</div>`);
-					// container.append(div1);
-					// container.append(div2);
-					// container.append(div3);
-					// container.append(div4);
-					// $('.result-container').append(container)
-				// });
-			// });
-		// });
-	// });
 });
 
 
@@ -58,9 +21,17 @@ function searchzip(zipcode) {
 	$.post("/api/recycling", {
 	  zipcode: zipcode
 	}).then(function(data) {
-	  console.log("hitting it?")
 	  console.log(data);
-	 //where we want the code to print to the page
+	 var container = $('<article>');
+	var div1 = $(`<div>${ data.description }</div>`);
+	var div2 = $(`<div>${ data.address }</div>`);
+	var div3 = $(`<div>${ data.city }</div>`);
+	var div4 = $(`<div>${ data.country }</div>`);
+	container.append(div1);
+	container.append(div2);
+	container.append(div3);
+	container.append(div4);
+	$('.result-container').append(container)
 	}).catch(function(err) {
 		console.log("maps");
 	  console.log(err.responseText);
